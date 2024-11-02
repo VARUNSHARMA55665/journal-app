d913e379c6983f6a7f04f952bf90d36392603448
varunsharma4 <varunsharma@pacefin.in>
2024-11-02 16:48:58 +0530

    sping boot project till rest apis crud operations created

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -0,0 +1,33 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public  void saveEntry(JournalEntry journalEntry) {
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAll() {
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> findById(Object id) {
+        return journalEntryRepository.findById(id);
+    }
+
+    public boolean deleteById(Object id){
+        journalEntryRepository.deleteById(id);
+        return true;
+    }
+
+}
