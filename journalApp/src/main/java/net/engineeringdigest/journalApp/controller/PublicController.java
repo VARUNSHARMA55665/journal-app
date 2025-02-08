f6ca3eb1fabcb793b6bc3f9e7a3d1d4536f0a57e
varunsharma4 <varunsharma@pacefin.in>
2025-02-08 20:38:39 +0530

    added logging with logback and slf4j

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -1,13 +1,19 @@
 package net.engineeringdigest.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
+
+    public static final Logger logger = LoggerFactory.getLogger(PublicController.class);
 
     @Autowired
     private UserService userService;
@@ -19,6 +25,9 @@ public class PublicController {
 
     @GetMapping("/health-check")
     public String healthCheck() {
+        logger.info("hahahahaha");
+        logger.warn("warning");
+        logger.error("errorerrorerrorerroro");
         return "Ok";
     }
 }
